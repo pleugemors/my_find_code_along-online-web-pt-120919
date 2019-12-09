b57ea3888b0b3415279f0706ff5bbb00c1411f3e
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 22:08:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0 
   while i < collection.length do 
     if yield(collection[i])
-      rerturn collection[i]
+      return collection[i]
     end
     i += 1 
   end
